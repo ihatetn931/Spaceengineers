420f28f77c06413e9ccad51ea248dfec249b64de
ihatetn931 <shadowpot@gmail.com>
2020-01-21 16:34:40 -0600

    Intital upload

---
+++ DrillCarAirLock.cs
@@ -0,0 +1,73 @@
+﻿using Sandbox.ModAPI.Ingame;
+using SpaceEngineers.Game.ModAPI.Ingame;
+using System.Collections.Generic;
+
+using System;
+
+
+namespace IngameScript
+{
+    partial class DrillCarAirLock : MyGridProgram
+    {
+        public DrillCarAirLock()
+        {
+            // Configure this program to run the Main method every 100 update ticks
+            Runtime.UpdateFrequency = UpdateFrequency.Update1;
+            return;
+        }
+        public void Main()
+        {
+            string outputText;
+            var airVent = GridTerminalSystem.GetBlockWithName("AirLockVent") as IMyAirVent;
+            var airLockOutterDoor = GridTerminalSystem.GetBlockWithName("AirlockOutterDoor") as IMyAirtightSlideDoor;
+            var airLockInnderDoorCockpit = GridTerminalSystem.GetBlockWithName("AirlockInnerDoorCockpit") as IMyAirtightSlideDoor;
+            var airLockInnderDoorProcessing = GridTerminalSystem.GetBlockWithName("AirlockInnerDoorProcessing") as IMyAirtightSlideDoor;
+            IMyBlockGroup lcdPanelsGroup = GridTerminalSystem.GetBlockGroupWithName("AirLockPanels");
+            var oxygenLevel = airVent.GetOxygenLevel();
+
+            if (lcdPanelsGroup == null)
+            {
+                Echo("Lcd Panels not found");
+                return;
+            }
+
+            List<IMyTextPanel> textPanels = new List<IMyTextPanel>();
+            lcdPanelsGroup.GetBlocksOfType(textPanels);
+            foreach (var textPanel in textPanels)
+            {
+                outputText = airVent.CustomName + "'s Pressure: " + String.Format("{0:P2}", oxygenLevel);
+                textPanel.WriteText(
+                    outputText + "\n" +
+                    "Pressure Unedited: " + oxygenLevel + "\n" +
+                    "Inner Door Cockpit: " + airLockInnderDoorCockpit.Status + "\n" +
+                    "Inner Door Processing Room: " + airLockInnderDoorCockpit.Status + "\n" +
+                    "Outter Door Open: " + airLockOutterDoor.Status + "\n" +
+                    "Vent Status: " + airVent.Status);
+            }
+            if (airVent.Depressurize)
+            {
+                airLockInnderDoorCockpit.CloseDoor();
+                airLockInnderDoorProcessing.CloseDoor();
+                airLockOutterDoor.Enabled = true;
+                if (oxygenLevel <= 0.0)
+                {
+                    airLockInnderDoorCockpit.Enabled = false;
+                    airLockInnderDoorProcessing.Enabled = false;
+                    airLockOutterDoor.OpenDoor();
+                }
+            }
+            else if (airVent.PressurizationEnabled)
+            {
+                airLockOutterDoor.CloseDoor();
+                airLockInnderDoorCockpit.Enabled = true;
+                airLockInnderDoorProcessing.Enabled = true;
+                if (oxygenLevel >= 1.0f)
+                {
+                    airLockOutterDoor.Enabled = false;
+                    airLockInnderDoorCockpit.OpenDoor();
+                    airLockInnderDoorProcessing.OpenDoor();
+                }
+            }
+        }
+    }
+}
---
+++ DrillCarDrill.cs
@@ -0,0 +1,88 @@
+﻿using Sandbox.ModAPI.Ingame;
+using System.Collections.Generic;
+
+namespace IngameScript
+{
+    partial class DrillCarDrill : MyGridProgram
+    {
+        public DrillCarDrill()
+        {
+            Runtime.UpdateFrequency = UpdateFrequency.Update1;
+        }
+        public void Main()
+        {
+            var panel = GridTerminalSystem.GetBlockWithName("DrillInfo") as IMyTextPanel;
+            var centerRotor = GridTerminalSystem.GetBlockWithName("Center Rotor") as IMyMotorAdvancedStator;
+            IMyBlockGroup group = GridTerminalSystem.GetBlockGroupWithName("Drill Pistons");
+            IMyBlockGroup drillGroup = GridTerminalSystem.GetBlockGroupWithName("Drills");
+            IMyBlockGroup warnLightGroup = GridTerminalSystem.GetBlockGroupWithName("RotatingLights");
+            if (group == null)
+            {
+                Echo("Group not found");
+                return;
+            }
+            if (panel == null)
+            {
+                Echo("Panel not found");
+                return;
+            }
+            if (drillGroup == null)
+            {
+                Echo("Drills not found");
+                return;
+            }
+            if (warnLightGroup == null)
+            {
+                Echo("Warn Lights not found");
+                return;
+            }
+            //Echo($"{group.Name}:");
+            List<IMyPistonBase> blocks = new List<IMyPistonBase>();
+            List<IMyShipDrill> drills = new List<IMyShipDrill>();
+            List<IMyLightingBlock> rLights = new List<IMyLightingBlock>();
+            group.GetBlocksOfType(blocks);
+            drillGroup.GetBlocksOfType(drills);
+            warnLightGroup.GetBlocksOfType(rLights);
+            foreach (var block in blocks)
+            {
+                foreach (var drill in drills)
+                {
+                    foreach (var rlight in rLights)
+                    {
+                        //Echo($"- {block.CustomName}");
+                        panel.WriteText(
+                            "Piston Range: " + block.CurrentPosition + "\n" +
+                            "Piston Status: " + block.Status + "\n" +
+                            "Piston Speed: " + block.Velocity + "\n" +
+                            "Drills Enabled: " + drill.Enabled + "\n" +
+                            "Rotor Locked: " + centerRotor.RotorLock);
+
+                        Echo(block.MaxLimit.ToString());
+                        if(drill.Enabled)
+                        {
+                            rlight.Enabled = true;
+                        }
+                        else
+                        {
+                            rlight.Enabled = false;
+                        }
+                        if (block.CurrentPosition == block.MaxLimit)
+                        {
+                            block.Velocity = 0.5f;
+                            block.Retract();
+                        }
+                        else 
+                        {
+                            if (block.CurrentPosition == block.MinLimit)
+                            {
+                                drill.Enabled = false;
+                                centerRotor.RotorLock = true;
+                                block.Velocity = -0.0105f;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
